2944b0fd8dd09329c16ad8ef439b74c0b032b9eb
spapadop <sokpao@gmail.com>
2019-04-15 21:20:18 +0200

    finished task 7.2 - crawling movies

--- nytscraper/spiders/imdb.py
+++ nytscraper/spiders/imdb.py
@@ -2,10 +2,12 @@ import scrapy
 import unidecode
 import re
 
-cleanString = lambda x: '' if x is None else unidecode.unidecode(re.sub(r'\s+',' ',x))
+cleanString = lambda x: '' if x is None else unidecode.unidecode(re.sub(r'\s+', ' ', x))
+
 
 class imdb(scrapy.Spider):
     name = 'imdb'
+    movies = set()
 
     def start_requests(self):
         allowed_domains = ['www.imdb.com']
@@ -13,7 +15,12 @@ class imdb(scrapy.Spider):
         yield scrapy.Request(url=start_url, callback=self.parse)
 
     def parse(self, response):
-        movieId = response.url.split("/")[-3]
+        movie_id = response.url.split("/")[-3]
+        res = self.movies.add(movie_id)
+        print(str(res))
+        if (self.movies.add(movie_id) == -1):
+            return
+        actors = []
         parent = response.css("div.parent").css('h3')
         title = parent.css('a::text').get()
         title = cleanString(title).strip()
@@ -24,19 +31,41 @@ class imdb(scrapy.Spider):
         for tr in cast_list.css('tr'):
             name = tr.css('td').css('a::text').get()
             name = cleanString(name).strip()
-            if(name):
+            if (name):
                 actor_id = tr.css('td').css('a[href]').get()
                 actor_id = actor_id.split('/')[2]
                 role = tr.css('td.character').css('a::text').get()
+                role = cleanString(role).strip()
                 if (not role):
                     role = tr.css('td.character::text').get()
                     role = cleanString(role).strip()
-                    
+
                 yield {
-                    "movie_id": movieId,
+                    "movie_id": movie_id,
                     "movie_name": title,
                     "movie_year": year,
                     "actor_name": name,
                     "actor_id": actor_id,
                     "role_name": role
                 }
+                actors.append(actor_id)
+
+        for actor in actors:
+            yield response.follow('https://www.imdb.com/name/' + actor, self.parse_actor)
+
+    def parse_actor(self, response):
+
+        entries = response.css('div.filmo-category-section').css('div')
+        for movie in entries:
+            try:
+                year = movie.css('span.year_column::text').get()
+                year = int(year.strip())
+            except:
+                continue
+
+            if 1980 <= year < 1990:
+                movie_id = movie.css('b').css('a[href]').get().split('/')[2]
+                type = movie.css('::text')
+                if not "TV" in str(type): # to remove TV Series and TV Movies from our dataset
+                    if movie_id:
+                        yield response.follow('http://www.imdb.com/title/' + movie_id + '/fullcredits/', self.parse)
